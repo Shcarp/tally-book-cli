99dce2138c50fdc17a7cc90876e2b6a8f1e7e550
sh-2020 <2191523412@qq.com>
2021-08-10 11:18:52 +0800

    q

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: 'root',
+      user: 'ubuntu',
       host: '1.117.56.86',
-      ref: 'origin/master',
-      repo: 'git@git.zhlh6.cn:Nick930826/juejue-vite-h5.git',
-      path: '/workspace/juejue-vite-h5',
+      ref: 'origin/main',
+      repo: 'git@github.com:sh-2020/tally-book-cli.git',
+      path: '~/workspace/tally-book',
       'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
